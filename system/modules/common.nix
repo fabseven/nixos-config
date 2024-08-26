0a8b81350686263cf7b3512b6f23026fa9b42f6a
dk <dk@debu>
2024-08-26 14:07:13 +0300

    missing stuff

--- system/modules/common.nix
+++ system/modules/common.nix
@@ -1,15 +1,7 @@
 { outputs, ... }: {
   nixpkgs.config.allowUnfree = true;
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
-    ];
-  };
+  nixpkgs = {};
 
   imports = [
     ./coding.nix
--- system/rb/default.nix
+++ system/rb/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, inputs, lib ... }: {
   imports = [
     ../modules/common.nix
     ../modules/bluetooth.nix
