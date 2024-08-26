b34e3761889e789f643f0fe2f1c98289e9bacbb5
dk <dk@debu>
2024-08-26 15:20:55 +0300

    kek

--- system/rb/hardware.nix
+++ system/rb/hardware.nix
@@ -7,7 +7,7 @@
   imports = [ (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "nvme", "xhci_pci", "usb_storage", "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
