f3edbdcfd2827ad42513e1b71d9e8120d41b45ba
dk <dk@debu>
2024-08-29 11:20:31 +0300

    xd

--- home/modules/swaylock.nix
+++ home/modules/swaylock.nix
@@ -1,7 +1,7 @@
 { pkgs, config, nix-colors, ... }:
 
 let
-  colorScheme = nix-colors.colorSchemes.paraiso;
+  colorScheme.palette.= nix-colors.colorSchemes.paraiso;
 in {
   home.file.".config/swaylock/config".text = ''
     daemonize
@@ -16,19 +16,19 @@ in {
     indicator-thickness=10
     indicator-radius=120
     hide-keyboard-layout
-    ring-color=${colorScheme.base0D}
-    key-hl-color=${colorScheme.base00}
+    ring-color=${colorScheme.palette.base0D}
+    key-hl-color=${colorScheme.palette.base00}
     line-color=00000000
     inside-color=00000088
     inside-clear-color=00000088
     separator-color=00000000
-    ring-ver-color=${colorScheme.base04}
+    ring-ver-color=${colorScheme.palette.base04}
     inside-ver-color=00000000
-    text-color=${colorScheme.base05}
-    text-ver-color=${colorScheme.base05}
-    text-clear-color=${colorScheme.base05}
-    bs-hl-color=${colorScheme.base0F}
-    ring-clear-color=${colorScheme.base0F}
+    text-color=${colorScheme.palette.base05}
+    text-ver-color=${colorScheme.palette.base05}
+    text-clear-color=${colorScheme.palette.base05}
+    bs-hl-color=${colorScheme.palette.base0F}
+    ring-clear-color=${colorScheme.palette.base0F}
     font=JetBrains Mono
   '';
 }
