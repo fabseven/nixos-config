21529612f7922e0bc60ad0858ead0b29b2f4c011
dk <dk@debu>
2024-08-29 11:18:59 +0300

    lol?

--- home/modules/swaylock.nix
+++ home/modules/swaylock.nix
@@ -16,19 +16,19 @@ in {
     indicator-thickness=10
     indicator-radius=120
     hide-keyboard-layout
-    ring-color=${palette.base0D}
-    key-hl-color=${palette.base00}
+    ring-color=${colorScheme.base0D}
+    key-hl-color=${colorScheme.base00}
     line-color=00000000
     inside-color=00000088
     inside-clear-color=00000088
     separator-color=00000000
-    ring-ver-color=${palette.base04}
+    ring-ver-color=${colorScheme.base04}
     inside-ver-color=00000000
-    text-color=${palette.base05}
-    text-ver-color=${palette.base05}
-    text-clear-color=${palette.base05}
-    bs-hl-color=${palette.base0F}
-    ring-clear-color=${palette.base0F}
+    text-color=${colorScheme.base05}
+    text-ver-color=${colorScheme.base05}
+    text-clear-color=${colorScheme.base05}
+    bs-hl-color=${colorScheme.base0F}
+    ring-clear-color=${colorScheme.base0F}
     font=JetBrains Mono
   '';
 }
