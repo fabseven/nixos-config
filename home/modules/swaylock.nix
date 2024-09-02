60a2dbd7b45affa3ec8170324dc32932a07e7cf3
fabi <fabbycrafted@gmail.com>
2024-09-02 16:36:08 +0300

    swaylock stuff

--- home/modules/swaylock.nix
+++ home/modules/swaylock.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       color = lib.mkDefault "111111";
-      image = lib.mkForce "../wallpaper.jpg";
+      image = lib.mkForce "../digital.jpg";
       ignore-empty-password = true;
       indicator-radius = "100";
       indicator-thickness = "10";
