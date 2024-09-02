78b0f53ca2f63c738ed94ddc27fcfac965cc4086
fabi <fabbycrafted@gmail.com>
2024-09-02 15:43:03 +0300

    swaylock stuff

--- home/modules/swaylock.nix
+++ home/modules/swaylock.nix
@@ -2,28 +2,28 @@
   programs.swaylock = {
     enable = true;
     settings = {
-      color = "111111";
+      lib.mkForce.color = "111111";
       ignore-empty-password = true;
       indicator-radius = "100";
       indicator-thickness = "10";
-      inside-clear-color = "222222";
-      inside-color = "1d2021";
-      inside-ver-color = "ff99441c";
-      inside-wrong-color = "ffffff1c";
-      key-hl-color = "ffffff80";
-      line-clear-color = "00000000";
-      line-color = "00000000";
-      line-ver-color = "00000000";
-      line-wrong-color = "00000000";
-      ring-clear-color = "ff994430";
-      ring-color = "282828";
-      ring-ver-color = "ffffff00";
-      ring-wrong-color = "ffffff55";
-      separator-color = "22222260";
-      text-caps-lock-color = "00000000";
-      text-clear-color = "222222";
-      text-ver-color = "00000000";
-      text-wrong-color = "00000000";
+      lib.mkForce.inside-clear-color = "222222";
+      lib.mkForce.inside-color = "1d2021";
+      lib.mkForce.inside-ver-color = "ff99441c";
+      lib.mkForce.inside-wrong-color = "ffffff1c";
+      lib.mkForce.key-hl-color = "ffffff80";
+      lib.mkForce.line-clear-color = "00000000";
+      lib.mkForce.line-color = "00000000";
+      lib.mkForce.line-ver-color = "00000000";
+      lib.mkForce.line-wrong-color = "00000000";
+      lib.mkForce.ring-clear-color = "ff994430";
+      lib.mkForce.ring-color = "282828";
+      lib.mkForce.ring-ver-color = "ffffff00";
+      lib.mkForce.ring-wrong-color = "ffffff55";
+      lib.mkForce.separator-color = "22222260";
+      lib.mkForce.text-caps-lock-color = "00000000";
+      lib.mkForce.text-clear-color = "222222";
+      lib.mkForce.text-ver-color = "00000000";
+      lib.mkForce.text-wrong-color = "00000000";
     };
   };
 }
