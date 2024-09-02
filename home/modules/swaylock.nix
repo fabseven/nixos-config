e77ce28403d7b37deed00c35042c8ae4b51fbe70
fabi <fabbycrafted@gmail.com>
2024-09-02 15:36:29 +0300

    swaylock stuff

--- home/modules/swaylock.nix
+++ home/modules/swaylock.nix
@@ -1,36 +1,29 @@
-{ pkgs, config, nix-colors, ... }:
-
-let
-  colorScheme = nix-colors.colorSchemes.paraiso;
-  wallpaper = ../digital.jpg;
-in {
-  home.file.".config/swaylock/config".text = ''
-    daemonize
-    clock
-    indicator
-    datestr=%a, %B %e
-    timestr=%I:%M %p
-    effect-blur=5x5
-    wallpaper=${wallpaper}
-    indicator-caps-lock
-    show-failed-attempts
-    ignore-empty-password
-    indicator-thickness=10
-    indicator-radius=120
-    hide-keyboard-layout
-    ring-color=${colorScheme.palette.base0D}
-    key-hl-color=${colorScheme.palette.base00}
-    line-color=00000000
-    inside-color=00000088
-    inside-clear-color=00000088
-    separator-color=00000000
-    ring-ver-color=${colorScheme.palette.base04}
-    inside-ver-color=00000000
-    text-color=${colorScheme.palette.base05}
-    text-ver-color=${colorScheme.palette.base05}
-    text-clear-color=${colorScheme.palette.base05}
-    bs-hl-color=${colorScheme.palette.base0F}
-    ring-clear-color=${colorScheme.palette.base0F}
-    font=JetBrains Mono
-  '';
+{ config, ... }: {
+  home-manager.users.${config.user}.programs.swaylock = {
+    enable = true;
+    settings = {
+      color = "111111";
+      ignore-empty-password = true;
+      indicator-radius = "100";
+      indicator-thickness = "10";
+      inside-clear-color = "222222";
+      inside-color = "1d2021";
+      inside-ver-color = "ff99441c";
+      inside-wrong-color = "ffffff1c";
+      key-hl-color = "ffffff80";
+      line-clear-color = "00000000";
+      line-color = "00000000";
+      line-ver-color = "00000000";
+      line-wrong-color = "00000000";
+      ring-clear-color = "ff994430";
+      ring-color = "282828";
+      ring-ver-color = "ffffff00";
+      ring-wrong-color = "ffffff55";
+      separator-color = "22222260";
+      text-caps-lock-color = "00000000";
+      text-clear-color = "222222";
+      text-ver-color = "00000000";
+      text-wrong-color = "00000000";
+    };
+  };
 }
