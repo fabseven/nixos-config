3f6e6c88aaf2d6be07c4e18d3827b1d3eb5ddd89
fabi <fabbycrafted@gmail.com>
2024-09-02 14:17:52 +0300

    helix stuff

--- home/modules/helix.nix
+++ home/modules/helix.nix
@@ -1,53 +1,232 @@
-{ config, pkgs, ... }: {
-
-home.packages = with pkgs; [
-      bash-language-server
-      biome
-      clang-tools
-      docker-compose-language-service
-      dockerfile-language-server-nodejs
-      golangci-lint
-      golangci-lint-langserver
-      gopls
-      gotools
-      helix-gpt
-      marksman
-      nil
-      nixpkgs-fmt
-      nodePackages.prettier
-      nodePackages.typescript-language-server
-      pgformatter
-      (python3.withPackages (p: (with p; [
-        black
-        isort
-        python-lsp-black
-        python-lsp-server
-      ])))
-      rust-analyzer
-      taplo
-      taplo-lsp
-      terraform-ls
-      typescript
-      vscode-langservers-extracted
-      yaml-language-server
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    bash-language-server
+    biome
+    clang-tools
+    docker-compose-language-service
+    dockerfile-language-server-nodejs
+    golangci-lint
+    golangci-lint-langserver
+    gopls
+    gotools
+    helix-gpt
+    marksman
+    nil
+    nixpkgs-fmt
+    nodePackages.prettier
+    nodePackages.typescript-language-server
+    pgformatter
+    (python3.withPackages (p: (with p; [
+      black
+      isort
+      python-lsp-black
+      python-lsp-server
+    ])))
+    rust-analyzer
+    taplo
+    taplo-lsp
+    terraform-ls
+    typescript
+    vscode-langservers-extracted
+    yaml-language-server
   ];
 
   programs.helix = {
-      languages = {
-        language = [
-          {
-            name = "go";
-            language-servers = [ "gopls" "golangci-lint-lsp" "gpt"];
-            formatter = {
-              command = "goimports";
-            };
-            auto-format = true;
-          }
-        ];
-      };
+    languages = {
+      language = [
+        {
+          name = "go";
+          language-servers = ["gopls" "golangci-lint-lsp" "gpt"];
+          formatter = {
+            command = "goimports";
+          };
+          auto-format = true;
+        }
+        {
+          name = "css";
+          language-servers = ["vscode-css-language-server" "gpt"];
+          formatter = {
+            command = "prettier";
+            args = ["--stdin-filepath" "file.css"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "html";
+          language-servers = ["vscode-html-language-server" "gpt"];
+          formatter = {
+            command = "prettier";
+            args = ["--stdin-filepath" "file.html"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "javascript";
+          language-servers = [
+            {
+              name = "typescript-language-server";
+              except-features = ["format"];
+            }
+            "biome"
+            "gpt"
+          ];
+          auto-format = true;
+        }
+        {
+          name = "json";
+          language-servers = [
+            {
+              name = "vscode-json-language-server";
+              except-features = ["format"];
+            }
+            "biome"
+          ];
+          formatter = {
+            command = "biome";
+            args = ["format" "--indent-style" "space" "--stdin-file-path" "file.json"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "jsonc";
+          language-servers = [
+            {
+              name = "vscode-json-language-server";
+              except-features = ["format"];
+            }
+            "biome"
+          ];
+          formatter = {
+            command = "biome";
+            args = ["format" "--indent-style" "space" "--stdin-file-path" "file.jsonc"];
+          };
+          file-types = ["jsonc" "hujson"];
+          auto-format = true;
+        }
+        {
+          name = "jsx";
+          language-servers = [
+            {
+              name = "typescript-language-server";
+              except-features = ["format"];
+            }
+            "biome"
+            "gpt"
+          ];
+          formatter = {
+            command = "biome";
+            args = ["format" "--indent-style" "space" "--stdin-file-path" "file.jsx"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "markdown";
+          language-servers = ["marksman" "gpt"];
+          formatter = {
+            command = "prettier";
+            args = ["--stdin-filepath" "file.md"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "nix";
+          formatter = {
+            command = "nixpkgs-fmt";
+          };
+          auto-format = true;
+        }
+        {
+          name = "python";
+          language-servers = ["pylsp" "gpt"];
+          formatter = {
+            command = "sh";
+            args = ["-c" "isort --profile black - | black -q -"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "rust";
+          language-servers = ["rust-analyzer" "gpt"];
+          auto-format = true;
+        }
+        {
+          name = "scss";
+          language-servers = ["vscode-css-language-server" "gpt"];
+          formatter = {
+            command = "prettier";
+            args = ["--stdin-filepath" "file.scss"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "sql";
+          language-servers = ["gpt"];
+          formatter = {
+            command = "pg_format";
+            args = ["-iu1" "--no-space-function" "-"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "toml";
+          language-servers = ["taplo"];
+          formatter = {
+            command = "taplo";
+            args = ["fmt" "-o" "column_width=120" "-"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "tsx";
+          language-servers = [
+            {
+              name = "typescript-language-server";
+              except-features = ["format"];
+            }
+            "biome"
+            "gpt"
+          ];
+          formatter = {
+            command = "biome";
+            args = ["format" "--indent-style" "space" "--stdin-file-path" "file.tsx"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "typescript";
+          language-servers = [
+            {
+              name = "typescript-language-server";
+              except-features = ["format"];
+            }
+            "biome"
+            "gpt"
+          ];
+          formatter = {
+            command = "biome";
+            args = ["format" "--indent-style" "space" "--stdin-file-path" "file.ts"];
+          };
+          auto-format = true;
+        }
+        {
+          name = "yaml";
+          language-servers = ["yaml-language-server"];
+          formatter = {
+            command = "prettier";
+            args = ["--stdin-filepath" "file.yaml"];
+          };
+          auto-format = true;
+        }
+      ];
+    };
   };
 
   # I love TOML configs more
-  home.file.".config/helix".source = config.lib.file.mkOutOfStoreSymlink
+  home.file.".config/helix".source =
+    config.lib.file.mkOutOfStoreSymlink
     "${config.home.homeDirectory}/nixos-config/helix";
 }
