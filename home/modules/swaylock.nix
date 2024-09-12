4a0320a72fd2011f28f296e17e09b51f11317bc5
M80 <fabbycrafted@gmail.com>
2024-09-12 13:17:09 +0300

    weird

--- home/modules/swaylock.nix
+++ home/modules/swaylock.nix
@@ -6,52 +6,35 @@ in {
   home.file.".config/swaylock/config".text = ''
     daemonize
     font=JetBrains Mono
-
-    wallpaper=${wallpaper}
-
-    screenshots
-
-    fade-in=0.2
-
-    effect-blur=20x2
-    #effect-greyscale
-    effect-scale=0.3
-
-    indicator
-    indicator-radius=240
-    indicator-thickness=20
-    indicator-caps-lock
-
-    key-hl-color=880033
-
-    separator-color=00000000
-
-    inside-color=00000099
-    inside-clear-color=ffd20400
-    inside-caps-lock-color=009ddc00
-    inside-ver-color=d9d8d800
-    inside-wrong-color=ee2e2400
-
-    ring-color=231f20D9
-    ring-clear-color=231f20D9
-    ring-caps-lock-color=231f20D9
-    ring-ver-color=231f20D9
-    ring-wrong-color=231f20D9
-
+    font-size 10
+    color=1e1e2e
+    bs-hl-color=f5e0dc
+    caps-lock-bs-hl-color=f5e0dc
+    caps-lock-key-hl-color=a6e3a1
+    inside-color=00000000
+    inside-clear-color=00000000
+    inside-caps-lock-color=00000000
+    inside-ver-color=00000000
+    inside-wrong-color=00000000
+    key-hl-color=a6e3a1
+    layout-bg-color=00000000
+    layout-border-color=00000000
+    layout-text-color=cdd6f4
     line-color=00000000
-    line-clear-color=ffd204FF
-    line-caps-lock-color=009ddcFF
-    line-ver-color=d9d8d8FF
-    line-wrong-color=ee2e24FF
-
-    text-clear-color=ffd20400
-    text-ver-color=d9d8d800
-    text-wrong-color=ee2e2400
-
-    bs-hl-color=ee2e24FF
-    caps-lock-key-hl-color=ffd204FF
-    caps-lock-bs-hl-color=ee2e24FF
-    disable-caps-lock-text
-    text-caps-lock-color=009ddcv
+    line-clear-color=00000000
+    line-caps-lock-color=00000000
+    line-ver-color=00000000
+    line-wrong-color=00000000
+    ring-color=b4befe
+    ring-clear-color=f5e0dc
+    ring-caps-lock-color=fab387
+    ring-ver-color=89b4fa
+    ring-wrong-color=eba0ac
+    separator-color=00000000
+    text-color=cdd6f4
+    text-clear-color=f5e0dc
+    text-caps-lock-color=fab387
+    text-ver-color=89b4fa
+    text-wrong-color=eba0ac
   '';  
 }
