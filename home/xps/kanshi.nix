c644c3c3254387f2d6ef4b6bcd44b890d59b8e04
M80 <fabbycrafted@gmail.com>
2024-09-24 17:37:50 +0300

    kanshi

--- home/xps/kanshi.nix
+++ home/xps/kanshi.nix
@@ -23,6 +23,7 @@
             criteria = "Samsung Electric Company LS27A800U HNMR601813";
             mode = "2560x1440@60Hz";
             position = "2560,0";
+						transform = "90";
             scale = 1.0;
           }
           {
