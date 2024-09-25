e601e8555e00c5818ab1876b435e23bc4a8d1962
M80 <fabbycrafted@gmail.com>
2024-09-25 15:37:35 +0300

    mako

--- home/modules/sway.nix
+++ home/modules/sway.nix
@@ -188,9 +188,19 @@ with config.lib.stylix.colors.withHashtag; {
   };
 
   services.mako = {
+    font = "JetBrains Mono";
+    icons = true;
     enable = true;
     defaultTimeout = 5000;
+    backgroundColor= "#1e1e2e";
+    textColor = "#cdd6f4";
+    borderColor = "#89b4fa";
+    progressColor = "#313244";
     ignoreTimeout = false;
+    extraConfig = ''
+      [urgency=high]
+      border-color=#fab387
+    ''
   };
 
  #services.swaync = {
