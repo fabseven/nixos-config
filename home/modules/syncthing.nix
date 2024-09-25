1111f306219967fd64a8a73add86f2670efa648f
fabi <fabbycrafted@gmail.com>
2024-09-25 18:45:07 +0300

    syncthing

--- home/modules/syncthing.nix
+++ home/modules/syncthing.nix
@@ -7,8 +7,5 @@
   services.syncthing = {
     enable = true;
     user = "dk";
-    settings.devices = {
-      "PC-1" = {id = "T6BERIU-M6YNR65-MNGIANL-5E7HR52-7X6IKKX-IIUULMY-PMJ37M7-53COKQX";};
-    };
   };
 }
