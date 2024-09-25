52737406d4a76cc3deab821b5358b874849a3bc2
M80 <fabbycrafted@gmail.com>
2024-09-25 13:42:38 +0300

    swaync

--- home/modules/sway.nix
+++ home/modules/sway.nix
@@ -196,8 +196,28 @@ with config.lib.stylix.colors.withHashtag; {
   services.swaync = {
     enable = true;
     settings = {
-      positionX = "left";
-      positionY = "bottom"; 
+      positionX = "right";
+      positionY = "top"; 
+      control-center-margin-top = 10;
+      control-center-margin-bottom = 10;
+      control-center-margin-right = 10;
+      control-center-margin-left = 10;
+      notification-icon-size = 64;
+      notification-body-image-height = 100;
+      notification-body-image-width = 200;
+      timeout = 10;
+      timeout-low = 5;
+      timeout-critical = 0;
+      fit-to-screen = false;
+      control-center-width = 500;
+      control-center-height = 1025;
+      notification-window-width = 500;
+      keyboard-shortcuts = true;
+      image-visibility = "when-available";
+      transition-time = 200;
+      hide-on-clear = false;
+      hide-on-action = true;
+      script-fail-notify = true;
     };
   };
 
