b8bfa19c79ef4ca7b0f86e9a4479af2adeaff59c
M80 <fabbycrafted@gmail.com>
2024-09-26 11:55:14 +0300

    kurwa

--- system/modules/users.nix
+++ system/modules/users.nix
@@ -2,7 +2,7 @@
   users.users = {
     dk = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "video" "nginx" "onepassword" "audio" "vboxusers"];
+      extraGroups = [ "wheel" "video" "nginx" "onepassword" "audio" "vboxusers" "networkmanager"];
       homeMode = "750"; # for nginx to read assets
     };
   };
