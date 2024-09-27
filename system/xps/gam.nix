b568684353fc9ef495391d2668ba4d8342f4cc19
M80 <fabbycrafted@gmail.com>
2024-09-27 15:56:59 +0300

    gam

--- system/xps/gam.nix
+++ system/xps/gam.nix
@@ -1,15 +1,24 @@
 { config, pkgs, ... }:
 
 let
-  gam = pkgs.callPackage ../modules/gam.nix {};
+  gam = import ./gam.nix {
+    inherit (pkgs) lib fetchFromGitHub python3;
+  };
 in
 {
+  # Your existing configuration...
+
   environment.systemPackages = with pkgs; [
+    # Your existing packages...
     gam
   ];
+
+  # If you want to make gam available to all users, you might want to add:
   environment.variables = {
     GAMUSERCONFIGDIR = "$XDG_CONFIG_HOME/gam";
     GAMSITECONFIGDIR = "$XDG_CONFIG_HOME/gam";
     GAMCACHEDIR = "$XDG_CACHE_HOME/gam";
   };
+
+  # Your other configuration options...
 }
