dc225a91dd8ee6f549b9201ca0c424b0a72c8a84
M80 <fabbycrafted@gmail.com>
2024-09-27 15:57:44 +0300

    gam

--- system/xps/gam.nix
+++ system/xps/gam.nix
@@ -6,19 +6,5 @@ let
   };
 in
 {
-  # Your existing configuration...
-
-  environment.systemPackages = with pkgs; [
-    # Your existing packages...
-    gam
-  ];
-
-  # If you want to make gam available to all users, you might want to add:
-  environment.variables = {
-    GAMUSERCONFIGDIR = "$XDG_CONFIG_HOME/gam";
-    GAMSITECONFIGDIR = "$XDG_CONFIG_HOME/gam";
-    GAMCACHEDIR = "$XDG_CACHE_HOME/gam";
-  };
-
-  # Your other configuration options...
+  environment.systemPackages = [ gam ];
 }
