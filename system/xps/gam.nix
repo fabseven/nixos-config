c351c5488eab5b106f632f66fd2d2577db8a865f
M80 <fabbycrafted@gmail.com>
2024-09-27 15:32:45 +0300

    gam

--- system/xps/gam.nix
+++ system/xps/gam.nix
@@ -9,9 +9,9 @@
      ];
 
      environment.variables = {
-       GAMUSERCONFIGDIR = "$XDG_CONFIG_HOME/gam";
-       GAMSITECONFIGDIR = "$XDG_CONFIG_HOME/gam";
-       GAMCACHEDIR = "$XDG_CACHE_HOME/gam";
+       GAMUSERCONFIGDIR = "$XDG_CONFIG_HOME/.gam";
+       GAMSITECONFIGDIR = "$XDG_CONFIG_HOME/.gam";
+       GAMCACHEDIR = "$XDG_CACHE_HOME/.gam";
      };
 
    }
