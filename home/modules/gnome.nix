36c9bcf4092fa8002aca61805b5e070b36dc2bb5
meow <meow@bibimbap.takaya-scylla.ts.net>
2024-12-19 20:28:00 +0200

    gnome

--- home/modules/gnome.nix
+++ home/modules/gnome.nix
@@ -1,11 +1,5 @@
 { pkgs, config, ... }: {
   dconf = {
     enable = true;
-    settings = {
-      "org/gnome/desktop/interface" = {
-            clock-show-weekday = true;
-            color-scheme = "prefer-dark";
-      };
-    };
   };
 }
