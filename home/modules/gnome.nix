625e9074644b0b937153a734c254f8cae0c4847e
dk <dk@dk.local>
2024-12-24 12:09:03 +0200

    gnome extensions + shell

--- home/modules/gnome.nix
+++ home/modules/gnome.nix
@@ -2,11 +2,26 @@
   dconf = {
     enable = true;
 		settings = {
+			"org/gnome/desktop/interface/".mkForce = {
+				color-scheme = "prefer-dark";
+			};
+
 			"org/gnome/shell" = {
 				disable-user-extensions = false;
+
+				favorite-apps = [
+					"spotify.desktop"
+				];
 				
 				enabled-extensions = [
 					"appindicatorsupport@rgcjonas.gmail.com"
+					"AlphabeticalAppGrid@stuarthayhurst"
+					"blur-my-shell@aunetx"
+					"just-perfection-desktop@just-perfection"
+					"notification-position@drugo.dev"
+					"space-bar@luchrich"
+					"tactile@lundal.io"
+					"undecorate@sun.wxg@gmail.com"
 				];
 			};
 		};
