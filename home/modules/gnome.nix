f7fde249f5abc180ae8208c11de4892f2f4c6a74
dk <dk@dk.local>
2024-12-25 00:15:56 +0200

    tweaking gnome

--- home/modules/gnome.nix
+++ home/modules/gnome.nix
@@ -20,15 +20,48 @@ in
 					"appindicatorsupport@rgcjonas.gmail.com"
 				];
 			};
+
 			"org/gnome/desktop/input-sources" = {
-				sources = [
+				#TODO: fix this poop
+				/* sources = [
 					"'xkb', 'us'"
 					"'xkb', 'fi'"
-				];
+				]; */
 				xkb-options = [
-					"caps:super" # map capslock to windows/mac key
-					"f:XF86AudioRaiseVolume"
+					"caps:ctrl_modifier" # map capslock to windows/mac key
 				];
+			};
+
+			"org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+				binding = "<Shift><Super>s";
+				command = "${flameshot-gui}/bin/flameshot-gui";
+				name = "flameshot screenshot";
+			};
+
+			"org/gtk/gtk4/settings/file-chooser" = {
+				sort-directories-first = true;
+			};
+
+			"org/gnome/nautilus/list-view" = {
+				use-tree-view = true;
+			};
+
+			"org/gnome/nautilus/preferences" = {
+				click-policy = "single";
+			};
+
+			"org/gnome/nautilus/preferences" = {
+				show-delete-permanently = true;
+			};
+
+			"org/gnome/desktop/interface" = {
+				clock-format = "24h";
+				enable-hot-corners = false;
+				show-battery-percentage = true;
+			};
+
+			"org/gnome/desktop/peripherals/touchpad" = {
+				tap-to-click = true;
 			};
 		};
 	};
