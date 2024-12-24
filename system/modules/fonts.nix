cc147559e0690ab5008b786bb9cc371ceb1e87c9
dk <dk@dk.local>
2024-12-24 21:44:10 +0200

    font issue

--- system/modules/fonts.nix
+++ system/modules/fonts.nix
@@ -3,9 +3,8 @@
   fonts.packages = with pkgs; [
     font-awesome # installed for waybar icons
     jetbrains-mono # terminal
-		iosevka
+		# iosevka
     # iosevka-bin
-		(nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
     # (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
     # (nerdfonts.override { fonts = [ "FiraMono" "JetBrainsMono" ]; })
   ];
