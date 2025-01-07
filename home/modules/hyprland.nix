d84ee9684f13297a03ef2d6e28a435de0c3b384b
kimbap <fabbycrafted@gmail.com>
2025-01-07 14:09:55 +0200

    comment

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -1,4 +1,5 @@
 {pkgs, config, ...}: {
+  # I am too lazy to write a nix config for hyprland
   home.file.".config/hypr".source = config.lib.file.mkOutOfStoreSymlink
     "${config.home.homeDirectory}/nixos-config/hypr";
 }
