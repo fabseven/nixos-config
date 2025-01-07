f2a9a9e8a9541221cbe5332645d275803ef0b578
kimbap <fabbycrafted@gmail.com>
2025-01-07 14:18:22 +0200

    waybar config from hyperland

---
+++ home/modules/waybar-hl.nix
@@ -0,0 +1,8 @@
+{pkgs, config, ...}: {
+  home.packages = with pkgs; [
+    waybar
+  ];
+
+  home.file.".config/waybar".source = config.lib.file.mkOutOfStoreSymlink
+    "${config.home.homeDirectory}/nixos-config/waybar-hl";
+}
