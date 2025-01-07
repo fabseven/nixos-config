fdd144440edbf1a5fa4f781d748daffcfd1a16b3
melon <fabbycrafted@gmail.com>
2025-01-07 21:32:21 +0200

    typo fix

--- system/modules/hyprland.nix
+++ system/modules/hyprland.nix
@@ -3,6 +3,6 @@
     enable = true;
     xwayland.enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackages = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 }
