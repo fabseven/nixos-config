ff24abfc78826ccc9cab1049c3e386a7832411a7
melon <fabbycrafted@gmail.com>
2025-01-07 23:15:59 +0200

    testing stylix

--- system/modules/stylix.nix
+++ system/modules/stylix.nix
@@ -6,18 +6,24 @@
 
 		polarity = "dark";
 
-    # https://tinted-theming.github.io/base16-gallery/
+    /* # https://tinted-theming.github.io/base16-gallery/
     base16Scheme = let theme = "catppuccin-mocha";
     in "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
 
     override = {
       base00 = "#1f1e22";
       base01 = "#232224";
-    };
+    }; */
 
     image = ../../home/wallpaper.jpg;
 
-		cursor.size = 18;
+/* 		cursor.size = 18; */
+
+		cursor = {
+			package = inputs.rose-pine-hyprcusror.packages.${pkgs.system}.default
+			name = "Rose Pine Hyprcursor"}
+			size = 24;
+		};
 
     fonts = rec {
       /* sansSerif = {
