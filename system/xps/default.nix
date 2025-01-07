22c1885d8404b47098741119c41744953fc0d28a
melon <fabbycrafted@gmail.com>
2025-01-07 21:51:40 +0200

    fixes again

--- system/xps/default.nix
+++ system/xps/default.nix
@@ -13,6 +13,10 @@
 				acpi 
 				mangohud
 		];
+		sessionVariables = {
+			WLR_NO_HARDWARE_CURSORS = "1";
+			NIXOS_OZONE_WL = "1";
+		};
 		localBinInPath = true;
 	};
 
@@ -40,7 +44,6 @@
 		};
 		hyprland = {
 			enable = true;
-			nvidiaPatches = true;
 			xwayland.enable = true;
 			package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
 			portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
