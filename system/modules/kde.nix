1b255e61803104870b5c4bcce9e259e5fd956443
Aleksi <fabbycrafted@gmail.com>
2025-01-13 21:43:18 +0200

    kde no worky

--- system/modules/kde.nix
+++ system/modules/kde.nix
@@ -1,21 +1,12 @@
 {pkgs, ...}: {
 
- #environment.plasma6.excludePackages = with pkgs.kdePackages; [
- #  plasma-browser-integration
- #  konsole
- #  elisa
- #];
+  environment.systemPackages = with pkgs; [
+    kdePackages.kate
+  ];
 
   services = {
     xserver.enable = true;
     displayManager.sddm.enable = true;
-    displayManager.sddm.wayland.enable = true;
     desktopManager.plasma6.enable = true;
-  };
-
-  qt = {
-    enable = true;
-    platformTheme = "gnome";
-    style = "adwaita-dark";
   };
 }
--- system/modules/sound.nix
+++ system/modules/sound.nix
@@ -5,6 +5,8 @@
 	security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
   };
 }
