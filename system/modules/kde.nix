c7a2ea880287f10cfa324ce911a76ee1e085d813
Aleksi <fabbycrafted@gmail.com>
2025-01-13 21:06:33 +0200

    small changes tokde

--- system/modules/kde.nix
+++ system/modules/kde.nix
@@ -1,14 +1,15 @@
 {pkgs, ...}: {
 
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    plasma-browser-integration
-    konsole
-    elisa
-  ];
+ #environment.plasma6.excludePackages = with pkgs.kdePackages; [
+ #  plasma-browser-integration
+ #  konsole
+ #  elisa
+ #];
 
   services = {
     xserver.enable = true;
-    displayManager.ly.enable = true;
+    displayManager.sddm.enable = true;
+    displayManager.sddm.wayland.enable = true;
     desktopManager.plasma6.enable = true;
   };
 
