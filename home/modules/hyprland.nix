19b1be2141b66f98ceb0e3e2fd65d9365e05ebb9
Aleksi <fabbycrafted@gmail.com>
2025-07-18 23:11:28 +0300

    oopsie x5

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -6,6 +6,31 @@
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    settings = {
+      exec-once = [
+        "systemctl --user start hyprpolkitagent"
+        "wl-clip-persist --clipboard regular & clipse -listen"
+      ];
+      input = {
+      kb_options = "ctrl:nocaps";
+      accel_profile = "flat";
+      sensitivity = 0.25;
+        touchpad = {
+          clickfinger_behavior = 2;
+          natural_scroll = true;
+          tap-to-click = true;
+          drag_lock = true;
+          disable_while_typing = true;
+          middle_button_emulation = false;
+        };
+      };
+      gestures = {
+        workspace_swipe = true;
+      };
+      bind = [
+        ",mouse:274, exec,"
+      ];
+    };
   };
 
   services.hyprpolkitagent.enable = true;
