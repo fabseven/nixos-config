3b17138e14655ad755126b63c20bb5c2fa9890e5
blk <blk@light.takaya-scylla.ts.net>
2025-09-12 12:09:08 +0300

    ♻️ (hyprland.nix): refactor Hyprland configuration by moving settings from home to system modules for better organization and maintainability

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -1,12 +1,8 @@
 { config, inputs, pkgs, ...}: {
 
   wayland.windowManager.hyprland = {
-    enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     settings = {
       exec-once = [
-        "systemctl --user start hyprpolkitagent"
         "wl-clip-persist --clipboard regular & clipse -listen"
       ];
       input = {
