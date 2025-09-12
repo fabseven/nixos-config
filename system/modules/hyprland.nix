25f26eac5e6d1b7c8cbe3b6e8182d48ae30f3b28
blk <blk@light.takaya-scylla.ts.net>
2025-09-12 12:00:09 +0300

    ✨ (hyprland.nix): introduce UWSM configuration for Hyprland compositor to enhance Wayland support and management

--- system/modules/hyprland.nix
+++ system/modules/hyprland.nix
@@ -19,15 +19,19 @@
     blueman.enable = true;
   };
   programs = {
-    uwsm.enable = true;
+    uwsm = {
+      enable = true;
+      waylandCompositors = {
+        hyprland = {
+          prettyName = "Hyprland";
+          comment = "Hyprland compositor managed by UWSM";
+          binPath = "/run/current-system/sw/bin/Hyprland";
+        };
+      };
+    };
     hyprland = {
       withUWSM = true;
     };
     nm-applet.enable = true;
-    # hyprland = {
-    #   enable = true;
-    #   package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    #   portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    # };
   };
 }
