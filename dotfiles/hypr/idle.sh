d4db5a37baea2e22a9199ecdf0f984d60d8ce9ac
blk <blk@light.takaya-scylla.ts.net>
2025-09-15 12:20:47 +0300

    ♻️ (hyprland.conf, idle.sh, sunset.sh): refactor scripts to use absolute paths for binaries to ensure consistency and reliability across different environments

--- dotfiles/hypr/idle.sh
+++ dotfiles/hypr/idle.sh
@@ -1,9 +1,14 @@
 #!/run/current-system/sw/bin/bash
 
-if pgrep -x hypridle >/dev/null; then
-  pkill -x hypridle
-  notify-send "Stop locking computer when idle"
+HYPRIDLE_BIN="/run/current-system/sw/bin/hypridle"
+NOTIFY_SEND="/run/current-system/sw/bin/notify-send"
+PGREP="/run/current-system/sw/bin/pgrep"
+PKILL="/run/current-system/sw/bin/pkill"
+
+if $PGREP -x hypridle >/dev/null; then
+  $PKILL -x hypridle
+  $NOTIFY_SEND "Stop locking computer when idle"
 else
-  uwsm app -- hypridle >/dev/null 2>&1 &
-  notify-send "Now locking computer when idle"
+  $HYPRIDLE_BIN >/dev/null 2>&1 &
+  $NOTIFY_SEND "Now locking computer when idle"
 fi
--- dotfiles/hypr/sunset.sh
+++ dotfiles/hypr/sunset.sh
@@ -1,22 +1,25 @@
-#!/usr/bin/env bash
+#!/run/current-system/sw/bin/bash
 
 # Default temperature values
 ON_TEMP=4000
 OFF_TEMP=6000
+HYPRCTL="/run/current-system/sw/bin/hyprctl"
+PGREP="/run/current-system/sw/bin/pgrep"
+NOTIFY_SEND="/run/current-system/sw/bin/notify-send"
 
 # Ensure hyprsunset is running
-if ! pgrep -x hyprsunset; then
+if ! $PGREP -x hyprsunset; then
   setsid uwsm app -- hyprsunset &
   sleep 1 # Give it time to register
 fi
 
 # Query the current temperature
-CURRENT_TEMP=$(hyprctl hyprsunset temperature 2>/dev/null | grep -oE '[0-9]+')
+CURRENT_TEMP=$($HYPRCTL hyprsunset temperature 2>/dev/null | grep -oE '[0-9]+')
 
 if [[ "$CURRENT_TEMP" == "$OFF_TEMP" ]]; then
-  hyprctl hyprsunset temperature $ON_TEMP
+  $HYPRCTL hyprsunset temperature $ON_TEMP
   notify-send "  Nightlight screen temperature"
 else
-  hyprctl hyprsunset temperature $OFF_TEMP
+  $HYPRCTL hyprsunset temperature $OFF_TEMP
   notify-send "   Daylight screen temperature"
 fi
